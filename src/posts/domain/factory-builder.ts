ae319e575d7108b291ce969e89de37b6d4fedb0f
Miguel Rodríguez <miguelrodrii@outlook.com>
2024-02-26 22:39:06 -0500

    Improved names under factory-builder

--- src/posts/domain/factory-builder.ts
+++ src/posts/domain/factory-builder.ts
@@ -1,12 +1,12 @@
 import type {Post} from './post';
 
-type factoryBuildDependencies = {
+type Dependencies = {
     isValid: (post: Post) => boolean;
 };
 
 export const buildMakePost =
-    ({isValid}: factoryBuildDependencies) =>
-    (post: Post): Post => {
+    ({isValid}: Dependencies) =>
+    (post: Post) => {
         if (!isValid(post)) {
             throw new Error('The given parameters are invalid.');
         }
